6333322109a9e6fdd9f8bfc386b035dba5160e54
Badre-s <bsahili@hotmail.fr>
2024-10-17 11:42:10 +0200

    Astroligie

---
+++ script.js
@@ -0,0 +1,64 @@
+// Here are our Astro signs provided as a string.
+
+const aries = `♈`,
+	taurus = `♉`,
+	gemini = `♊`,
+	cancer = `♋`,
+	leo = `♌`,
+	virgo = `♍`,
+	libra = `♎`,
+	scorpio = `♏`,
+	sagittarius = `♐`,
+	capricorn = `♑`,
+	aquarius = `♒`,
+	pisces = `♓︎`;
+
+// In Western Astrology there are 12 signs, organized by Earth Elements (Eart, Water, Air, Fire)
+// You are going to manipulate the following arrays along with this challenge:
+
+/*  🏁 Add one final element to an array 
+    Sagittarius is missing from fire signs please add it at the 
+    END of the array and verify the result.
+*/
+
+const fireSigns = ["♈", "♌", "♐"];
+
+console.log(
+	fireSigns[fireSigns.length - 1] === "♐"
+		? "Good Answer ✅"
+		: "Wrong Answer ❌",
+);
+
+/* 🏁 Remove the last element of an array
+      Sagittarius should not be on earth Signs, please remove 
+      Sagittarius from the array, and verify the result.
+*/
+
+const earthSigns = ["♉", "♍", "♑"];
+
+// the line below is for testing, don't touch it :)
+console.log(
+	earthSigns[earthSigns.length - 1] !== "♐"
+		? "Good Answer ✅"
+		: "Wrong Answer ❌",
+);
+
+/* 🏁 Remove one element at the begining of an array
+      Pisces should not be on air Signs, please remove Pisces 
+      from the array, and verify the result.
+*/
+
+const airSigns = ["♊", "♎", "♒"];
+
+// the line below is for testing, don't touch it :)
+console.log(airSigns[0] !== "♓︎" ? "Good Answer ✅" : "Wrong Answer ❌");
+
+/*  🏁 Add one element at the beginning of an array
+    Cancer is missing from water signs please add it at the 
+    BEGINNING of the array and verify the result.
+*/
+
+const waterSigns = ["♋", "♏", "♓︎"];
+
+// the line below is for testing, don't touch it :)
+console.log(waterSigns[0] === "♋" ? "Good Answer ✅" : "Wrong Answer ❌");
